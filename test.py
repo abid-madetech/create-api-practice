a4879e53ba3c6e9cebed7e388205fed3fe575f19
Abid <abdur.abid@madetech.com>
2024-12-10 12:35:07 +0000

    added one more failing test for courses

--- test.py
+++ test.py
@@ -11,3 +11,9 @@ def client():
 def test_request_example(client):
     response = client.get("/")
     assert response.status_code == 200
+
+def test_get_courses(client):
+    response = client.get("/courses")
+    assert response.status_code == 200
+
+    
