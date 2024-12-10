e21d37f9e9b7811dbcfeb14e5c2d51226c2983e5
Abid <abdur.abid@madetech.com>
2024-12-10 15:22:34 +0000

    tests passing, json file works

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask
+import json
 
-def create_app():
-    app = Flask(__name__)
-    return app
+app = Flask(__name__)
+
+with open("tests/courses.json", "r") as courses:
+    courses_json = json.load(courses)
+
+@app.route("/")
+def hello_world():
+    return "<h1>hello world<h1/>"
+
+@app.route("/courses")
+def hello_courses():
+    return courses_json
