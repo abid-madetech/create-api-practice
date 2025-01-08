e587ff430b3f0b2b5f12b19b84b3f083f489a5ab
Abid <abdur.abid@madetech.com>
2025-01-08 10:41:08 +0000

    use oidc

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,54 +1,54 @@
-resource "aws_ecs_cluster" "main" {
-    name = var.app_name
-}
+# resource "aws_ecs_cluster" "main" {
+#     name = var.app_name
+# }
 
-resource "aws_ecs_task_definition" "app" {
-  family                   = var.app_name
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  memory                   = "512"
-  cpu                      = "256"
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+# resource "aws_ecs_task_definition" "app" {
+#   family                   = var.app_name
+#   requires_compatibilities = ["FARGATE"]
+#   network_mode             = "awsvpc"
+#   memory                   = "512"
+#   cpu                      = "256"
+#   execution_role_arn       = aws_iam_role.ecs_task_execution.arn
 
-  container_definitions = jsonencode(
-    [
-      {
-        "name": "${var.app_name}",
-        "image": "261219435789.dkr.ecr.${var.aws_region}.amazonaws.com/${var.app_name}:latest",
-        "memory": 512,
-        "cpu": 256,
-        "portMappings": [
-          {
-            "containerPort": 8000,
-            "hostPort": 8000,
-            "protocol": "tcp"
-          }
-        ],
-        "logConfiguration": {
-          "logDriver": "awslogs",
-          "options": {
-            "awslogs-group": "/ecs/${var.app_name}",
-            "awslogs-region": "${var.aws_region}",
-            "awslogs-stream-prefix": "ecs"
-          }
-        }
-      }
-    ]
-  )
-}
+#   container_definitions = jsonencode(
+#     [
+#       {
+#         "name": "${var.app_name}",
+#         "image": "261219435789.dkr.ecr.${var.aws_region}.amazonaws.com/${var.app_name}:latest",
+#         "memory": 512,
+#         "cpu": 256,
+#         "portMappings": [
+#           {
+#             "containerPort": 8000,
+#             "hostPort": 8000,
+#             "protocol": "tcp"
+#           }
+#         ],
+#         "logConfiguration": {
+#           "logDriver": "awslogs",
+#           "options": {
+#             "awslogs-group": "/ecs/${var.app_name}",
+#             "awslogs-region": "${var.aws_region}",
+#             "awslogs-stream-prefix": "ecs"
+#           }
+#         }
+#       }
+#     ]
+#   )
+# }
 
 
 
-resource "aws_ecs_service" "app" {
-    name = var.app_name
-    cluster = aws_ecs_cluster.main.id
-    task_definition = aws_ecs_task_definition.app.arn
-    desired_count = 1
-    launch_type = "FARGATE"
+# resource "aws_ecs_service" "app" {
+#     name = var.app_name
+#     cluster = aws_ecs_cluster.main.id
+#     task_definition = aws_ecs_task_definition.app.arn
+#     desired_count = 1
+#     launch_type = "FARGATE"
 
-    network_configuration {
-      subnets = aws_subnet.default[*].id
-      security_groups = [aws_security_group.app.id]
-      assign_public_ip = true
-    }
-}
+#     network_configuration {
+#       subnets = aws_subnet.default[*].id
+#       security_groups = [aws_security_group.app.id]
+#       assign_public_ip = true
+#     }
+# }
